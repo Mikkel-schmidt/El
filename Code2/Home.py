64754b06535a16b956eeb51a1b938d455ba224d1
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2023-03-08 15:52:05 +0100

    vascd

--- Code2/Home.py
+++ Code2/Home.py
@@ -87,7 +87,7 @@ if check_password():
             Bar()
             .add_xaxis(list(df['tid']))
             .add_yaxis('Samlet forbrug', list(df['amount']), label_opts=opts.LabelOpts(is_show=False, formatter="{b}: {c}"),)
-            .reversal_axis()
+            #.reversal_axis()
             .set_global_opts(
                 datazoom_opts=[opts.DataZoomOpts(type_="inside", orient="vertical"), opts.DataZoomOpts(type_="slider", orient="vertical")], 
                 #legend_opts=opts.LegendOpts(orient='vertical', pos_left="left", is_show=True),
