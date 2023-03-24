6f68f5b90d892b99365f291b4a62af50a1905ab3
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2023-03-24 13:26:20 +0100

    vsd

--- Code2/Home.py
+++ Code2/Home.py
@@ -126,10 +126,10 @@ if check_password():
     #with col2:
     st.markdown('---')
     col1 , col2= st.columns([1,1])
-    col1.header('Top 10 driftsforbedringer')
+    col1.header('Top driftsforbedringer')
     col1.dataframe(df_besp[['Adresse', 'årligt forbrug',  '%']].head(10).style.background_gradient(cmap='Reds').set_precision(1), use_container_width=True )
     if 'df_g' in st.session_state:
-        col2.header('Top 10 standby forbedringer')
+        col2.header('Top standby forbedringer')
         col2.dataframe(st.session_state['df_g'].head(10).style.background_gradient(cmap='Blues').set_precision(1),use_container_width=True)
 
 
