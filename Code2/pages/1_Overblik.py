16a59cbc1157abdc3c809258a132728fc4e0ff40
Mikkel Rasmus Schmidt <misc@nrgi.dk>
2023-04-20 15:58:02 +0200

    nøgletal

--- Code2/pages/1_Overblik.py
+++ Code2/pages/1_Overblik.py
@@ -109,8 +109,8 @@ col1, col2 = st.columns([1,2])
 col1.subheader('Benchmark på tværs')
 col1.markdown("""Det bedste overblik over bygningerne fås ved at sammenligne deres forbrug.
 Ud fra dette kan det ses hvilke bygninger der er *mest energiintensive* og derfor hvilke bygninger der potentielt er noget at komme efter.""")
-col1.write(df_besp)
-col1.write(df_besp[['Adresse', 'årligt forbrug']].sort_values('årligt forbrug', ascending=False))
+df_besp['nøgletal'] = df_besp['årligt forbrug'] / df_besp['areal']
+col1.write(df_besp[['Adresse', 'årligt forbrug', 'areal', 'nøgletal', 'anvendeleskode']].sort_values('årligt forbrug', ascending=False))
 
 @st.cache_resource
 def barr(df, grader):
